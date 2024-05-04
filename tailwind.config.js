227a0d30cb0715aed35891e10813451101607839
RajaAdhikary2005 <rajaadhikary05032005@gmail.com>
2024-05-04 13:27:56 +0530

    1st-day

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontsize:{
+        s:"0.75rem",
+        b:"0.875rem",
+        m:"0.1rem",
+        l:"1.25rem",
+        xl:"1.5rem",
+        "2xl":"1.75rem",
+        "3xl":"2.25rem",
+      }
+    },
   },
   plugins: [],
 }
