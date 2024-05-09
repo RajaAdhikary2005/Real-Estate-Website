209ae302f5d328c667deca495b3729e92f3df178
RajaAdhikary2005 <rajaadhikary05032005@gmail.com>
2024-05-09 23:37:15 +0530

    09/05/2024

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,9 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
-      fontsize:{
-        s:"0.75rem",
-        b:"0.875rem",
-        m:"0.1rem",
-        l:"1.25rem",
-        xl:"1.5rem",
-        "2xl":"1.75rem",
-        "3xl":"2.25rem",
-      }
+
+      backgroundImage: {
+        'team-bg': "url('/team-bg-img.png')",}
     },
   },
   plugins: [],
